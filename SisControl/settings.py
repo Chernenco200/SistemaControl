adefb5c2be74797eb27b6dd040ea9db060640865
jerry <jerence2@hotmail.com>
2023-12-09 00:49:47 -0500

    9comit

--- SisControl/settings.py
+++ SisControl/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS=[
     'static')
 ]
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 
 if not DEBUG:
@@ -152,5 +152,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
